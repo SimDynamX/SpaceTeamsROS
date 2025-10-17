a274d9ebddf6ce6b2989a9e0abb644e9c7d71e5f
connorjak <connor.jakubik@gmail.com>
2025-10-17 01:18:43 -0500

    Disable Yotam's image server script for now, use image_client ros node instead

--- run_rosbridge.bash
+++ run_rosbridge.bash
@@ -41,11 +41,11 @@ ros2 launch rosbridge_server rosbridge_websocket_launch.xml &
 ROSBRIDGE_PID=$!
 echo "Started rosbridge server with PID $ROSBRIDGE_PID"
 
-# Start image server
-python3 scripts/Image_server.py &
-IMAGE_SERVER_PID=$!
-echo "Started image server with PID $IMAGE_SERVER_PID"
-echo "All services started. Press Ctrl+C to stop."
+# Start image server #DISABLED FOR NOW!
+# python3 scripts/Image_server.py &
+# IMAGE_SERVER_PID=$!
+# echo "Started image server with PID $IMAGE_SERVER_PID"
+# echo "All services started. Press Ctrl+C to stop."
 
 # Wait indefinitely until a signal is received
 while true; do
